c5b207bffe3391f3d7f3f233a55d80fd123298a2
airglow923 <p.hyundeok76@gmail.com>
2020-08-07 09:53:02 +0800

    more specific example

--- tag-dispatch.cpp
+++ tag-dispatch.cpp
@@ -5,15 +5,23 @@
 
 #include <type_traits>
 #include <iterator>
+#include <vector>
+#include <iostream>
 
 namespace hyundeok {
     template<typename Iter, typename BinaryOperation>
     void func(Iter beg, Iter end, BinaryOperation binary_op,
-        std::random_access_iterator_tag);
+        std::random_access_iterator_tag)
+    {
+        std::cout << "std::random_access_iterator\n";
+    }
 
     template<typename Iter, typename BinaryOperation>
     void func(Iter beg, Iter end, BinaryOperation binary_op,
-        std::forward_iterator_tag);
+        std::forward_iterator_tag)
+    {
+        std::cout << "std::forward_access_iterator\n";
+    }
 
     template<typename Iter, typename BinaryOperation>
     void func(Iter beg, Iter end, BinaryOperation binary_op)
@@ -21,4 +29,12 @@ namespace hyundeok {
         func(beg, end, binary_op,
             typename std::iterator_traits<Iter>::iterator_category{});
     }
+}
+
+int main()
+{
+    using namespace hyundeok;
+
+    std::vector<int> v{0, 1, 2, 3, 4, 5};
+    func(v.begin(), v.end(), []{});
 }
